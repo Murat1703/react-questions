5e57c1ba647181584877f3647b906ffed882b1f4
family <family@MacBook-Air-family.local>
2025-05-27 18:56:30 +0500

    badge component

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,11 +1,12 @@
 import cls from './HomePage.module.css'
 import { QuestionCard } from '../../components/questionCard'
 import { API_URL } from '../../constants';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { QuestionCardList } from '../../components/QuestionCardList';
 import { Loader } from '../../components/Loader';
 import { DelayFn } from '../../helpers/delayFn';
 import { useFetch } from '../../hooks/useFetch';
+import { SearchInput } from '../../components/SearchInput';
 
 
 const cards = [];
@@ -13,6 +14,8 @@ const cards = [];
 export function HomePage(){
 
     const [questions, setQuestions] = useState([]);
+    const [searchValue, setSearchValue] = useState("");
+
 
     const [getQuestions, isLoading, error] = useFetch(async (url)=>{
         const response = await fetch(`${API_URL}/${url}`);
@@ -45,8 +48,14 @@ export function HomePage(){
     }, [])
 
 
+    function onSearchChangeHandler(e){
+        setSearchValue(e.target.value);
+    }
     return(
         <>
+        <div className={cls.controlContainer}>
+            <SearchInput value={searchValue} onChange={onSearchChangeHandler}/>
+        </div>
             {isLoading &&<Loader/>}
             {error && <p>{error}</p>}            
             <QuestionCardList cards={questions}/>
