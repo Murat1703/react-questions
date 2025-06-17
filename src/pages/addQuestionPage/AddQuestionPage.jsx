2b3690f302c001120b911850b42b6eb54c60ab1b
family <family@MacBook-Air-family.local>
2025-06-17 16:40:28 +0500

    addquestion

--- src/pages/addQuestionPage/AddQuestionPage.jsx
+++ src/pages/addQuestionPage/AddQuestionPage.jsx
@@ -1,16 +1,60 @@
 import cls from './AddQuestionPage.module.css';
 import { Button } from '../../components/Button';
+import { useActionState } from 'react';
+import { DelayFn } from '../../helpers/delayFn';
+import { toast } from 'react-toastify';
+import { API_URL } from '../../constants';
+
+const createCardAction = async(_prevState, formData) => {
+    try {
+        await DelayFn();
+        console.log(Object.fromEntries(formData));
+
+        const newQuestion = Object.fromEntries(formData);
+        const resources = newQuestion.resources.trim();
+        const isClearForm = newQuestion.clearform;
+
+        const response = await fetch(`${API_URL}/react`, {
+            method: "POST",
+            body: JSON.stringify({
+                question: newQuestion.question,
+                answer: newQuestion.answer,
+                description: newQuestion.description,
+                resources: resources.length ? resources.split(','): [],
+                level: Number(newQuestion.level),
+                completed: false,
+                editDate: undefined
+            }),
+        })
+
+        if( response.status == 404){
+            throw new Error(response.statusText)
+        }
+
+        const question = response.json();
+        toast.success("New Questuion is Successfully created");
+        return isClearForm ? {} : question;
+    }
+    catch(error){
+        toast.error(error.message)
+        return {}
+    }
+
+}
 
 export const AddQuestionPage = () => {
+
+    const [formState, formAction, isPending]= useActionState(createCardAction, {clearForm: true});
+
     return( 
         <>
             <h1 className={cls.formTitle}>Add Question page</h1>
             <div className={cls.formContainer}>
-                <form action="" className={cls.form}>
+                <form action={formAction} className={cls.form}>
                     <div className={cls.formControl}>
                         <label htmlFor="questionField">Question: </label>
                         <textarea 
-                            defaultValue={"defaultValue"}
+                            defaultValue={formState.question}
                             name="question" 
                             id="questionField" 
                             cols={30} 
@@ -22,7 +66,7 @@ export const AddQuestionPage = () => {
                     <div className={cls.formControl}>
                         <label htmlFor="answerField">Short Answer: </label>
                         <textarea 
-                            defaultValue={"defaultValue"}
+                            defaultValue={formState.answer}
                             name="answer" 
                             id="answerField" 
                             cols={30} 
@@ -34,7 +78,7 @@ export const AddQuestionPage = () => {
                     <div className={cls.formControl}>
                         <label htmlFor="descriptionField">Description: </label>
                         <textarea 
-                            defaultValue={"defaultValue"}
+                            defaultValue={formState.description}
                             name="description" 
                             id="descriptionField" 
                             cols={30} 
@@ -46,7 +90,7 @@ export const AddQuestionPage = () => {
                     <div className={cls.formControl}>
                         <label htmlFor="resourcesField">Resources: </label>
                         <textarea 
-                            defaultValue={"defaultValue"}
+                            defaultValue={formState.resources}
                             name="resources" 
                             id="resourcesField" 
                             cols={30} 
@@ -57,7 +101,7 @@ export const AddQuestionPage = () => {
                     </div>
                     <div className={cls.formControl}>
                         <label htmlFor="levelField">Level: </label>
-                        <select name="level" id="levelField">
+                        <select name="level" id="levelField" defaultValue={formState.level}>
                             <option disabled>Question Level</option>
                             <hr />
                             <option value={1}>1 - easiest</option>
@@ -67,11 +111,11 @@ export const AddQuestionPage = () => {
                     </div>
 
                     <label htmlFor="clearFormField" className={cls.clearFormControl}>
-                        <input type="checkbox" name='clearForm' id='clearFormField' defaultChecked={true} className={cls.checkbox}/>
+                        <input type="checkbox" name='clearForm' id='clearFormField' defaultChecked={formState.clearForm} className={cls.checkbox}/>
                         <span>Clear Form After submitting?</span>
                     </label>
 
-                    <Button>Add Question</Button>
+                    <Button disabled={isPending}>Add Question</Button>
                 </form>
             </div>
         </>
